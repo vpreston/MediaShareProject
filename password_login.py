bc0690cf86674d6bd547d9e4aac37fa6589280be
Alexander Crease <doctor@TARDIS.(none)>
2013-12-14 11:50:23 -0500

    finished password stuff!

--- password_login.py
+++ password_login.py
@@ -1,27 +1,36 @@
 from swampy.Gui import *
+from getpass import *
 
 d={}
 
-def newuser(username,password1,password2):
-    signupgui=Gui()
-    signupgui.title('Sign Up')
-    signupgui.mainloop()
-    if username in d:
-        return "This username is already in use! Please enter a different one"
-    elif password1==password2:
-        d[username]=password1
-        return "Thank you for creating an account!"
+def newuser():
+    if newusername.get() in d:
+        label.config(text="That username is already in use.")
+    elif newpassword.get()==repeatnewpassword.get():
+        d[newusername.get()]=newpassword.get()
+        label.config(text="Thank you for signing up!")
     else:
-        return "Your passwords don't match! Please try again"
+        label.config(text="Your passwords don't match! Please try again")
 
-def login(user,password):
-    if password==d[user]:
+def login():
+    username=raw_input("MusicSwAPPer Username:  ")
+    password=getpass("MusicSwAPPer Password:  ")
+    if password==d[username]:
         return "You are now logged in!"
     else:
         return "We do not recognize your username or password, please try again."
 
-print newuser('acrease','kendall123','kendall123')
-print login('acrease','kendall123')
-print login('acrease','123kendall')        
+signupgui=Gui()
+signupgui.title('MusicSwAPPer Sign Up')
+usernamelabel=signupgui.la(text='Username:')
+newusername=signupgui.en()
+newpasswordlabel=signupgui.la(text='Password:')
+newpassword=signupgui.en(show='*')
+repeatpasswordlabel=signupgui.la(text='Re-enter Password')
+repeatnewpassword=signupgui.en(show='*')
+button=signupgui.bu(text='Sign Up!',command=newuser)
+label=signupgui.la()
 
-    
+signupgui.mainloop()
+
+print login()
