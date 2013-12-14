4d9eb172c15e401ab0f75dde2787521eb3b6ef8b
Alexander Crease <doctor@TARDIS.(none)>
2013-12-14 10:25:54 -0500

    fixing

--- mogoclienttester.py
+++ mogoclienttester.py
@@ -2,18 +2,12 @@ import pymongo
 import datetime
 
 from pymongo import MongoClient
-<<<<<<< HEAD
-client = MongoClient('snapcracklepop',27017)
-=======
-client = MongoClient('mongodb://alex:tester@dharma.mongohq.com:10075/mediashareproject')
+
+client = MongoClient('mongodb://alex:test@dharma.mongohq.com:10075/mediashareproject')
 
 
 db = client.mediashareproject
->>>>>>> 02e9c0c2f73e420a5f99ed8c01338b363ea20c08
-
-
 posts = db.posts
-
 
 print posts
 
